f44f6b0cacb6e2ab655fdf6c148de4a1f448f2ab
Jinggo Villamor <jinggo@securlinks.com>
2018-05-30 15:25:59 +0800

    Update sample

--- Sample.js
+++ Sample.js
@@ -1,11 +1,14 @@
 import { processQueue, app } from './Queue';
 
-const SampleProcess = (data, done) => {
-    console.log(data)
+processQueue((data, done) => {
+    console.log('Email',data)
     done();
-}
+}, 'email', 20);
 
-processQueue(SampleProcess, 'email', 20);
+processQueue((data, done) => {
+    console.log('SMS',data)
+    done();
+}, 'sms', 20);
 
 console.log(`Running QUEUE on port localhost:${process.env.PORT}`);
 app.listen(process.env.PORT);
