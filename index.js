4c85e28be80033bbf496def8ea57369046742cf5
Jinggo Villamor <jinggo@securlinks.com>
2018-05-30 14:58:53 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import { processQueue, app } from './src/Queue';
+
+const SampleProcess = (data, done) => {
+    console.log(data)
+    done();
+}
+
+processQueue(SampleProcess, 'email', 20);
+
+console.log(`Running QUEUE on port localhost:${process.env.PORT}`);
+app.listen(process.env.PORT);
---
+++ src/Queue.js
@@ -0,0 +1,25 @@
+import kue from 'kue';
+import basicAuth from 'basic-auth-connect';
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use(basicAuth(process.env.BASIC_AUTH_USERNAME,process.env.BASIC_AUTH_PASSWORD));
+app.use(kue.app);
+
+const queue = kue.createQueue();
+
+queue.setMaxListeners(1000);
+
+const processQueue = (func, type, n) => {
+    queue.process(type, n, (job, done) => {
+        func(job.data, done)
+    })
+}
+
+export {
+    processQueue,
+    app
+}
